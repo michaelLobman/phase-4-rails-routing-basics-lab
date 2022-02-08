10fd8502a2f33458ca9b2ac821c3cc7f6f619f60
Michael Lobman <michael.lobman@gmail.com>
2022-02-07 22:05:53 -0500

    commit tree 297a5ed8c99327fde59fea3dca20f3e58f8ddd40

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,15 @@
+class StudentsController < ApplicationController
+    def index
+        render json: Student.all
+    end
+
+    def grades
+        students = Student.order("grade DESC")
+        render json: students
+    end
+
+    def highest_grade
+        student = Student.order("grade DESC").first
+        render json: student
+    end
+end
